de9dc760a599d18d762c8e1ec2150f6764a51888
Kristian Espina <onelespina@gmail.com>
2021-07-14 08:25:51 +0800

    Updated Home Page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -18,10 +18,10 @@ const Home = () => {
             <div className="text-5xl text-indigo-600 pt-24 mb-4 font-bold">Looking for work?</div>
             <form className="flex flex-row gap-4">
               <div className="w-96">
-                <TextInput placeholder="Search for job title, or company" state={search} setValue={setSearch} />
+                <TextInput placeholder="Search for job title, or position" state={search} setValue={setSearch} />
               </div>
               <div>
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" type="submit">
                   Search
                 </button>
               </div>
